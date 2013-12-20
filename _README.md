aa7840677d7391fd4e63d019ef5ba9e48f054051
Sérgio Ramos <mail@sergioramos.me>
2013-12-19 16:48:50 -0800

    convert to component

---
+++ _README.md
@@ -0,0 +1,27 @@
+# hexrgb
+
+convert a hex string to a rgb array and back
+
+# install
+
+```bash
+component install ramitos/hexrgb
+```
+
+# example
+
+```js
+var hexrgb = require('../src/hexrgb')
+var assert = require('assert')
+
+assert.deepEqual(hexrgb.rgb('#fef'), [255, 238, 255, 1])
+assert.deepEqual(hexrgb.rgb('#fffFEF'), [255, 255, 239, 1])
+assert.deepEqual(hexrgb.rgb('rgb(244, 233, 100)'), [244, 233, 100, 1])
+assert.deepEqual(hexrgb.rgb('rgb(100%, 30%, 90%)'), [255, 77, 229, 1])
+assert.deepEqual(hexrgb.rgb('rgba(200, 20, 233, 0.2)'), [200, 20, 233, 0.2])
+assert.deepEqual(hexrgb.rgb('rgba(200, 20, 233, 0)'), [200, 20, 233, 0])
+assert.deepEqual(hexrgb.rgb('rgba(100%, 30%, 90%, 0.2)'), [255, 77, 229, 0.2])
+assert.deepEqual(hexrgb.rgb('rgba(300, 600, 100, 3)'), [255, 255, 100, 1])
+assert.deepEqual(hexrgb.rgb('rgba(8000%, 100%, 333%, 88)'), [255, 255, 255, 1])
+assert.equal(hexrgb.hex([255, 10, 35]), '#FF0A23')
+```
---
+++ src/hexrgb.js
@@ -0,0 +1,64 @@
+require('component-plus-node')
+var type = require('type')
+
+function scale (num, min, max) {
+  return Math.min(Math.max(min, num), max)
+}
+
+function hexDouble (num) {
+  var str = num.toString(16).toUpperCase()
+  return (str.length < 2) ? '0' + str : str
+}
+
+module.exports.rgb = function (string) {
+  if (type(string) !== 'string') return [];
+
+  var abbr =  /^#([a-fA-F0-9]{3})$/
+  var hex =  /^#([a-fA-F0-9]{6})$/
+  var rgba = /^rgba?\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(?:,\s*([\d\.]+)\s*)?\)$/
+  var per = /^rgba?\(\s*([\d\.]+)\%\s*,\s*([\d\.]+)\%\s*,\s*([\d\.]+)\%\s*(?:,\s*([\d\.]+)\s*)?\)$/
+
+  var rgb = [0, 0, 0], a = 1
+  var match = string.match(abbr)
+
+  if (match) {
+    match = match[1]
+    for (var i = 0; i < rgb.length; i+=1) {
+      rgb[i] = parseInt(match[i] + match[i], 16)
+    }
+  } else if (match = string.match(hex)) {
+    match = match[1]
+    for (var y = 0; y < rgb.length; y+=1) {
+      rgb[y] = parseInt(match.slice(y * 2, y * 2 + 2), 16)
+    }
+  } else if (match = string.match(rgba)) {
+    for (var z = 0; z < rgb.length; z+=1) {
+      rgb[z] = parseInt(match[z + 1])
+    }
+
+    a = parseFloat(match[4])
+  } else if (match = string.match(per)) {
+    for (var x = 0; x < rgb.length; x+=1) {
+      rgb[x] = Math.round(parseFloat(match[x + 1]) * 2.55)
+    }
+
+    a = parseFloat(match[4])
+  } else {
+    return [];
+  }
+
+  for (var k = 0; k < rgb.length; k+=1) {
+    rgb[k] = scale(rgb[k], 0, 255)
+  }
+
+  if (!a && a !== 0) a = 1
+  else a = scale(a, 0, 1)
+
+  rgb.push(a)
+  return rgb
+}
+
+module.exports.hex = function (rgb) {
+  if (type(rgb) !== 'array') return '';
+  return '#' + hexDouble(rgb[0]) + hexDouble(rgb[1]) + hexDouble(rgb[2])
+}
